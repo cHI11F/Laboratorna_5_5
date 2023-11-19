8c4a4a53077d7da207bc8a2712dd2265b6a5a2c4
cHI11F <nazar.vata@dspu.edu.ua>
2023-11-19 22:29:23 +0200

    initial commit

---
+++ PR19/PR19.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+double sequence(int n, double a) {
+    if (n == 1) {
+        return 8;
+    }
+    else {
+        return (8 - a) / n;
+    }
+}
+
+double findLimit(int n, double a, int depth) {
+    if (depth == 0) {
+        std::cout << "Глибина рекурсії досягла максимального значення." << std::endl;
+        return a;
+    }
+
+    double currentTerm = (8 - a) / n;
+    double limit = 8;
+
+    for (int i = 2; i <= n; ++i) {
+        currentTerm = (8 - currentTerm) / i;
+        limit += currentTerm;
+    }
+
+    return limit;
+}
+
+int main() {
+    int n;
+    std::cout << "Vvedіt' znachennya n: ";
+    std::cin >> n;
+
+
+    double initialTerm = 1.0;
+
+
+    double result = sequence(n, initialTerm);
+
+    std::cout << "Znachennya poslidovnostі a(" << n << ") : " << result << std::endl;
+
+    int recursionDepth = n;  
+    double limit = findLimit(n, initialTerm, recursionDepth);
+    std::cout << "Mezha poslidovnostі pri n → ∞: " << limit << std::endl;
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PR19/PR19.cpp" 
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+    TEST_CLASS(SequenceTest)
+    {
+    public:
+
+
+
+        TEST_METHOD(TestMethod2) {
+            int n = 5;
+            double a = 2.0;
+
+            
+            double expectedValue = -1.2; 
+
+            
+            Assert::AreEqual(sequence(n, a), expectedValue);
+        }
